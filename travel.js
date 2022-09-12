2429cef4695e91381e5e2ca6f4c8f94df8d07481
geekayush <contactayusharma@gmail.com>
2022-09-12 11:11:19 +0530

    added signup and login page

--- travel.js
+++ travel.js
@@ -1,11 +1,33 @@
 console.log("javascript")
-window.onload = function () {
-    var btn = document.getElementById('btn')
 
-    function myfunction() {
-        console.log("Hello World");
-    };
-    if (btn) {
-        btn.addEventListener("click", myfunction);
-    }
+var btn = document.getElementById('btn')
+
+function myfunction() {
+    document.querySelector('.signup').classList.add('active');
+    document.querySelector('.login').classList.remove('passive')
+};
+
+btn.addEventListener("click", myfunction);
+
+var cross = document.querySelector('.cross')
+
+function closefunction() {
+    document.querySelector('.signup').classList.remove('active');
 }
+cross.addEventListener("click", closefunction);
+
+var loginBtn = document.getElementById('log')
+
+function login() {
+    document.querySelector('.login').classList.add('passive')
+    document.querySelector('.signup').classList.remove('active')
+}
+loginBtn.addEventListener("click", login);
+
+var close = document.querySelector('.close')
+
+function crossfunction() {
+    document.querySelector('.login').classList.remove('passive');
+}
+close.addEventListener("click", crossfunction);
+
